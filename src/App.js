10bf3590a2cdc98bf236a36c6e2a6d3ffb05bb2b
Sangduen W <sangduen.w@kbtg.tech>
2021-04-21 15:54:38 +0700

    Working with list

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 import User from './components/User'
 import Clock from './components/Clock'
 import Form from './components/Form'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  users = [
+    { id: 1, name: "User 01", age: 30},
+    { id: 2, name: "User 02", age: 20},
+    { id: 3, name: "User 03", age: 10},
+  ];
+
+  render() {
+    return <div className="App">
       <header className="App-header">
         Hello React Ningchan
         <img src={logo} className="App-logo" alt="logo" />
 
         <div>
           <h1><Clock ></Clock></h1>
-          <ul>
-            <User info="User 01"></User>
-            <User info="User 02"></User>
-            <User info="User 03"></User>
-          </ul>
+          <User users={this.users} />
           <Form/>
         </div>
       </header>
     </div>
-  );
+  }
+
 }
 
 export default App;
--- src/components/User.js
+++ src/components/User.js
@@ -7,8 +7,12 @@ const { Component } = require("react");
 // export default User;
 
 class User extends Component{
+    
     render(){
-        return <li>{this.props.info}</li>;
+        const data = this.props.users.map((user)=>(
+            <li key={user.id}>{user.name} - {user.age}</li>
+          ));
+        return <ul>{data}</ul>;
     }
 }
 export default User;
